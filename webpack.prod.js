c0b18acb2ff5394ca895b393b170a72500c549ba
Luciano Frizzera <lucaju@me.com>
2019-12-31 11:31:25 -0500

    improve webpack config

---
+++ webpack.prod.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const TerserPlugin = require('terser-webpack-plugin');
+
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+	// mode: 'production', //development || production
+	entry: './src/app.js',
+	output: {
+		filename: '[name].bundle.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	devtool: false,
+	performance: {
+		hints: 'warning'
+	},optimization: {
+		namedModules: false,
+		namedChunks: false,
+		nodeEnv: 'production',
+		flagIncludedChunks: true,
+		occurrenceOrder: true,
+		sideEffects: true,
+		usedExports: true,
+		concatenateModules: true,
+		noEmitOnErrors: true,
+		checkWasmTypes: true,
+		minimize: true,
+		minimizer: [new TerserPlugin({
+			cache: true,
+			parallel: true,
+			sourceMap: false//true
+		})]
+	},
+	plugins: [
+		new webpack.DefinePlugin({'process.env.NODE_ENV': JSON.stringify('production')}),
+		new webpack.optimize.ModuleConcatenationPlugin(),
+		new webpack.NoEmitOnErrorsPlugin(),
+		new CopyWebpackPlugin([
+			{ from: './src/assets', to: 'assets' }
+		]),
+		new HtmlWebpackPlugin({
+			template: 'src/index.html',
+			inject: 'body'
+		}),
+	]
+});
