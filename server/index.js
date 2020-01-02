51a467bea76cf636a557c125adc90844c6fb789c
Luciano Frizzera <lucaju@me.com>
2020-01-02 12:08:17 -0500

    Use server routers

--- server/index.js
+++ server/index.js
@@ -1,32 +1,35 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 
-const dialogFlow = require('./dialogflow.js');
-const videoSelection = require('./video-selection.js');
+// const dialogFlow = require('./dialogflow.js');
+// const videoSelection = require('./video-selection.js');
+
+const dialogflowRouter = require('./routers/dialogflow');
+const videoRouter = require('./routers/video');
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({ extended: false })); // support encoded bodies
+app.use(bodyParser.urlencoded({
+	extended: false
+})); // support encoded bodies
 app.use(bodyParser.json()); // support json encoded bodies
+
 app.use(express.static('dist'));
 app.use('/videos', express.static(__dirname + '/videos'));
 app.use('/assets', express.static(__dirname + '/assets'));
 
+//routers
+app.use(dialogflowRouter);
+app.use(videoRouter);
 
-app.post('/dialogflow', async (req, res) => {
-	const msg = req.body.msg;
-	const dialogRes = await dialogFlow.sendDialog(msg);
-	res.send(dialogRes);
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+	const err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
-
-app.post('/getVideo', async (req, res) => {
-	const data = req.body;
-	const video = await videoSelection.getVideo(data);
-	res.json(video);
-});
-
 
 
 app.listen(port, () => console.log(`App listening on port ${port}!`));
